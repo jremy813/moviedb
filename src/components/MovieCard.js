bd8a05ed6860cee6e650709018723f8076206337
jremy813 <jremywatkins@gmail.com>
2020-08-30 16:36:16 -0400

    fixed movie card background images

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MovieCard.css";
 
 function MovieCard() {
-  return <div className="movieCard"></div>;
+  const [images, setImages] = useState([]);
+  const [titles, setTitles] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=fff872c9121905fd933cd3592aece9dd&language=en-US"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        console.log(data.results[0].poster_path);
+        const titles = data.results.map((title) => title.title);
+        const images = data.results.map((image) => image.backdrop_path);
+
+        setImages(images);
+        setTitles(titles);
+        console.log(titles);
+      });
+  }, []);
+
+  return (
+    <>
+      {images.map((img) => (
+        <div
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/w500/${img})`,
+          }}
+          className="movieCard"
+        ></div>
+      ))}
+    </>
+  );
 }
 
 export default MovieCard;
